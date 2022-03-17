3304f5e068ea5ef3de3d94072a63fbdf5f2c983f
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-03-16 22:35:43 -0700

    Data

---
+++ data.js
@@ -0,0 +1,32 @@
+/** @typedef {'USA' | 'France' | 'Switzerland' } Country */
+
+/** @typedef {{ readonly country: Country }} Record */
+
+/** @typedef {{ readonly[k in string]: Record}} Data */
+
+/** @type {Data} */
+const data = {
+    'Abbott Labs': { country: 'USA' },
+    'Accor': { country: 'France' },
+    'Hyat': { country: 'USA' },
+    'Marriott': { country: 'USA' },
+    'Amway': { country: 'USA' },
+    'Dunkin Donuts': { country: 'USA' },
+    'Cargill': { country: 'USA' },
+    'Citigroup': { country: 'USA' },
+    'General Mills': { country: 'USA' },
+    'Nestle': { country: 'Switzerland' },
+    'Halliburton': { country: 'USA' },
+    'Herbalife Nutrition': { country: 'USA' },
+    'Koch Industries': { country: 'USA' },
+    'Leo Burnett': { country: 'USA' },
+    'Subway': { country: 'USA' },
+    'Kraken': { country: 'USA' },
+    'Binance': { country: 'USA' },
+    'Coinbase': { country: 'USA' },
+}
+
+module.exports = {
+    /** @readonly */
+    data,
+}
