0cdb46df7d27a2a66ec28df7c1f38deeed98beb0
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-03-17 16:43:54 -0700

    table generation

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
 const data = require('./data.js')
 const list = require('functionalscript/types/list/index.js')
+const object = require('functionalscript/types/object/index.js')
 
-const table = () => {
-    for (const i in Object.entries(data)) {
+/** @type {(_: object.Entry<data.Record>) => (_: string) => string} */
+const tr = ([name, record]) => s => `${s}<tr><td>${name}</td><td>${record.country}</td></tr>`
 
-    }
-    return ''
-}
+const table = () => list.reduce(tr)('')(Object.entries(data.data))
 
 const html = `<html><body><table>${table()}</table></body></html>`
 
