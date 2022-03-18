a0867dc154e03f13672d3f4a0530f4689841a61c
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-03-17 20:59:01 -0700

    test

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const html = require('./html.js')
     const x = ['div', ['<div>&amp;</div>']]
     const s = html.elementToString(x)
     if (s !== '<div>&lt;div&gt;&amp;amp;&lt;/div&gt;</div>') { throw s }
-    console.log(s)
+    // console.log(s)
 }
 
-console.log(indexHtml)
+// console.log(indexHtml)
