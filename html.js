4e09955911dc8454b634db12f392dbed12f3d687
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-03-17 20:40:30 -0700

    HTML

--- html.js
+++ html.js
@@ -1,22 +1,27 @@
 const list = require('functionalscript/types/list/index.js')
-const operator = require('functionalscript/types/function/operator')
+const operator = require('functionalscript/types/function/operator/index.js')
+const { compose } = require('functionalscript/types/function/index.js')
 
 /** @typedef {readonly[string, list.List<Node>]} Element */
 
 /** @typedef {Element | string} Node */
 
+/** @type {(s: string) => list.List<string>} */
+const escape = s => [s];
+
 /** @type {(n: Node) => list.List<string>} */
-const node = n => typeof n === 'string' ? [n] : element(n)
+const node = n => typeof n === 'string' ? escape(n) : element(n)
 
 const nodes = list.flatMap(node)
 
 /** @type {(element: Element) => list.List<string>} */
 const element = ([tag, ns]) => list.flat([[`<`, tag, `>`], nodes(ns), [`</`, tag, `>`]])
 
-/** @type {(e: Element) => string} */
-const elementToString = e => list.fold(operator.concat)('')(element(e))
+const elementToString = compose(element)(list.fold(operator.concat)(''))
 
 module.exports = {
+    /** @readonly */
+    element,
     /** @readonly */
     elementToString,
 }
